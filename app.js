7632f232ef0372ee58955b0b810dae01a7dae0e5
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-01-20 15:44:22 -0800

    added working gif search

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // Require Libraries
 const express = require('express');
+// app.js
+// Require Libraries
+// Require tenorjs near the top of the file
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "S45OW5R6OOXA", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 
 // App Setup
 const app = express();
@@ -13,10 +23,21 @@ app.set('view engine', 'handlebars');
 // Routes
 // ROUTES
 // example URL "http://localhost:3000/?term=hey"
+// Routes
 app.get('/', (req, res) => {
-        console.log(req.query) // => "{ term: hey" }
-
-        res.render('home')
+        // Handle the home page when we haven't queried yet
+        term = ""
+        if (req.query.term) {
+            term = req.query.term
+        }
+        // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+        Tenor.Search.Query(term, "10")
+            .then(response => {
+                // store the gifs we get back from the search
+                const gifs = response;
+                // pass the gifs as an object into the home page
+                res.render('home', { gifs })
+            }).catch(console.error);
     })
     // Routes
 app.get('/greetings/:name', (req, res) => {
